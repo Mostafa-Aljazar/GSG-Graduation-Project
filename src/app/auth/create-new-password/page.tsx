ac530b73310d2ebaf4646e2893450db5fc45ff64
Mostafa-Aljazar <mostafaibrahim20032020@gmail.com>
2025-11-27 15:20:31 +0200

    fix: wrap the CreateNewPasswordComponent by Suspense to solve hydration error

--- src/app/auth/create-new-password/page.tsx
+++ src/app/auth/create-new-password/page.tsx
@@ -1,5 +1,10 @@
 import CreateNewPasswordComponent from '@/components/auth/create-new-password-component';
+import { Suspense } from 'react';
 
 export default function CreateNewPasswordPage() {
-  return <CreateNewPasswordComponent />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CreateNewPasswordComponent />
+    </Suspense>
+  );
 }
