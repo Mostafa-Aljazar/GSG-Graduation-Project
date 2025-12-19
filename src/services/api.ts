8bd521442e17b755533a55be788f34a03407bd9c
Mostafa-Aljazar <mostafaibrahim20032020@gmail.com>
2025-12-19 10:56:48 +0200

    fix: change NEXT_PUBLIC_APP_URL in env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import { clearSessionCookie, getSessionCookie } from '@/utils/auth/cookies/serverCookies'
 import axios from 'axios'
 
-const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000/api'
+const baseURL = `${process.env.NEXT_PUBLIC_APP_URL}+/api` || 'http://localhost:3000/api'
 
 const defaultConfig = {
   baseURL,
